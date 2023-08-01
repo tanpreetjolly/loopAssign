0856e8eede54b9813f9a6fcd49f67ac56ad12e6b
TanpreetJolly <tanpreetjolly@pop-os.localdomain>
2023-08-01 10:50:21 +0530

    push

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,6 +1,7 @@
 import dataset_small from "../data/dataset_small.json";
 import Multiselect from "multiselect-react-dropdown";
 import Table from "./Table";
+import { useMemo, useState } from "react";
 
 type Data = {
   mod3: number;
@@ -10,18 +11,58 @@ type Data = {
   number: number;
 };
 const dataObj: Data[] = dataset_small;
-const Filters = () => {
+
+const getFiltersValues = (data: Data[]) => {
   function getUniqueValues(arr: Data[], property: keyof Data) {
     const uniqueValuesSet = new Set(arr.map((item) => item[property]));
     return Array.from(uniqueValuesSet, (value) => ({ value, display: value }));
   }
 
-  const numbers = getUniqueValues(dataObj, "number");
-  const mod3Values = getUniqueValues(dataObj, "mod3");
-  const mod4Values = getUniqueValues(dataObj, "mod4");
-  const mod5Values = getUniqueValues(dataObj, "mod5");
-  const mod6Values = getUniqueValues(dataObj, "mod6");
+  const numbers = getUniqueValues(data, "number").slice(0, 10);
+  const mod3Values = getUniqueValues(data, "mod3");
+  const mod4Values = getUniqueValues(data, "mod4");
+  const mod5Values = getUniqueValues(data, "mod5");
+  const mod6Values = getUniqueValues(data, "mod6");
   console.log(mod3Values);
+  return [numbers, mod3Values, mod4Values, mod5Values, mod6Values];
+};
+
+const filterData = (data: Data[], filters: any) => {
+  return data.filter((item) => {
+    return (
+      (filters.number.length === 0 || filters.number.includes(item.number)) &&
+      (filters.mod3.length === 0 || filters.mod3.includes(item.mod3)) &&
+      (filters.mod4.length === 0 || filters.mod4.includes(item.mod4)) &&
+      (filters.mod5.length === 0 || filters.mod5.includes(item.mod5)) &&
+      (filters.mod6.length === 0 || filters.mod6.includes(item.mod6))
+    );
+  });
+};
+    
+
+const Filters = () => {
+  const [filters, setFilters] = useState({
+    number: [],
+    mod3: [],
+    mod4: [],
+    mod5: [],
+    mod6: [],
+  });
+  console.log(filters);
+
+  const [numbers, mod3Values, mod4Values, mod5Values, mod6Values] = useMemo(
+    () => getFiltersValues(dataObj),
+    [dataObj]
+  );
+
+  const data = useMemo(() => filterData(dataObj, filters), [filters]);
+
+  const handleFilterChange = (filterName: string, selectedList: any[]) => {
+    const filter = selectedList.map((item) => item.value);
+    setFilters({ ...filters, [filterName]: filter });
+    
+  };
+
   return (
     <div>
       <div className="container" style={{ display: "flex", gap: "10px" }}>
@@ -29,30 +70,60 @@ const Filters = () => {
           options={numbers}
           displayValue="display"
           placeholder="Number"
+          onSelect={(selectedList) => {
+            handleFilterChange("number", selectedList);
+          }}
+          onRemove={(selectedList) => {
+            handleFilterChange("number", selectedList);
+          }}
         />
         <Multiselect
           options={mod3Values}
           displayValue="display"
           placeholder="mod3"
+          onSelect={(selectedList) => {
+            handleFilterChange("mod3", selectedList);
+          }}
+          onRemove={(selectedList) => {
+            handleFilterChange("mod3", selectedList);
+          }}
         />
         <Multiselect
           options={mod4Values}
           displayValue="display"
           placeholder="mod4"
+          onSelect={(selectedList) => {
+            handleFilterChange("mod4", selectedList);
+          }}
+          onRemove={(selectedList) => {
+            handleFilterChange("mod4", selectedList);
+          }}
         />
         <Multiselect
           options={mod5Values}
           displayValue="display"
           placeholder="mod5"
+          onSelect={(selectedList) => {
+            handleFilterChange("mod5", selectedList);
+          }}
+          onRemove={(selectedList) => {
+            handleFilterChange("mod5", selectedList);
+          }}
         />
         <Multiselect
           options={mod6Values}
           displayValue="display"
           placeholder="mod6"
+          onSelect={(selectedList) => {
+            handleFilterChange("mod6", selectedList);
+          }}
+          onRemove={(selectedList) => {
+            handleFilterChange("mod6", selectedList);
+          }}
         />
       </div>
       <div>
-        <Table />
+        <Table tableData={data} />
       </div>
     </div>
   );
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,74 +1,154 @@
-import React from "react";
-import DataTable from "react-data-table-component";
-import data from "../data/dataset_small.json";
+import 
+ { useState, useEffect, useMemo, useCallback } from "react";
+import Multiselect from "multiselect-react-dropdown";
+import Table from "./Table";
+import dataset_small from "../data/dataset_small.json";
 
-interface DataItem {
-  number: number;
+type Data = {
   mod3: number;
   mod4: number;
   mod5: number;
   mod6: number;
-}
-
-interface ColumnType {
-  name: string;
-  selector: (row: DataItem) => number | string;
-  sortable: boolean;
-}
-
-const columns: ColumnType[] = [
-  {
-    name: "Number",
-    selector: (row) => row.number,
-    sortable: true,
-  },
-  {
-    name: "Mod 3",
-    selector: (row) => row.mod3,
-    sortable: true,
-  },
-  {
-    name: "Mod 4",
-    selector: (row) => row.mod4,
-    sortable: true,
-  },
-  {
-    name: "Mod 5",
-    selector: (row) => row.mod5,
-    sortable: true,
-  },
-  {
-    name: "Mod 6",
-    selector: (row) => row.mod6,
-    sortable: true,
-  },
-];
-
-const paginationOptions = {
-  rowsPerPageText: "Rows per page:",
-  rangeSeparatorText: "of",
-  selectAllRowsItem: true,
-  selectAllRowsItemText: "All",
+  number: number;
 };
 
-const DataTableComponent: React.FC = () => {
-  const tableData: DataItem[] = data;
+const Filters = () => {
+  const dataObj: Data[] = dataset_small;
+
+  const [filters, setFilters] = useState({
+    number: [],
+    mod3: [],
+    mod4: [],
+    mod5: [],
+    mod6: [],
+  });
+
+  // Filter Data based on selected filters
+  const filterData = (data: Data[], filters: any) => {
+    return data.filter((item) => {
+      return (
+        (filters.number.length === 0 || filters.number.includes(item.number)) &&
+        (filters.mod3.length === 0 || filters.mod3.includes(item.mod3)) &&
+        (filters.mod4.length === 0 || filters.mod4.includes(item.mod4)) &&
+        (filters.mod5.length === 0 || filters.mod5.includes(item.mod5)) &&
+        (filters.mod6.length === 0 || filters.mod6.includes(item.mod6))
+      );
+    });
+  };
+
+  const filteredData = useMemo(
+    () => filterData(dataObj, filters),
+    [dataObj, filters]
+  );
+
+  const handleFilterChange = (filterName: string, selectedList: any[]) => {
+    const filter = selectedList.map((item) => item.value);
+    setFilters((prevFilters) => ({ ...prevFilters, [filterName]: filter }));
+  };
+
+  const handleFilterInteractions = useCallback(() => {
+    if (filters.mod3.length > 0) {
+      const selectedMod3Values = filters.mod3.map(Number);
+      const mod6Options = Array.from(
+        new Set(
+          dataObj
+            .filter((data) => selectedMod3Values.includes(data.mod3))
+            .map((data) => data.mod6.toString())
+        )
+      );
+      setFilters((prevFilters) => ({ ...prevFilters, mod6: mod6Options }));
+    }
+  }, [dataObj, filters.mod3]);
+
+  useEffect(() => {
+    handleFilterInteractions();
+  }, [filters.mod3, handleFilterInteractions]);
+
+  const getUniqueValues = (property: keyof Data) => {
+    const uniqueValuesSet = new Set(dataObj.map((item) => item[property]));
+    return Array.from(uniqueValuesSet, (value) => ({ value, display: value }));
+  };
+
+  const numbers = useMemo(() => getUniqueValues("number"), [dataObj]);
+  const mod3Values = useMemo(() => getUniqueValues("mod3"), [dataObj]);
+  const mod4Values = useMemo(() => getUniqueValues("mod4"), [dataObj]);
+  const mod5Values = useMemo(() => getUniqueValues("mod5"), [dataObj]);
+  const mod6Values = useMemo(
+    () =>
+      getUniqueValues("mod6").filter((option) =>
+        filters.mod6.includes(option.value)
+      ),
+    [dataObj, filters.mod6]
+  );
+
   return (
     <div>
-      <DataTable
-        columns={columns}
-        data={tableData}
-        pagination
-        paginationPerPage={100}
-        paginationRowsPerPageOptions={[100]}
-        paginationComponentOptions={paginationOptions}
-        paginationTotalRows={tableData.length}
-        fixedHeader
-        fixedHeaderScrollHeight="500px"
-        responsive  
-      />
+      <div className="container" style={{ display: "flex", gap: "10px" }}>
+        <Multiselect
+          options={numbers}
+          displayValue="display"
+          placeholder="Number"
+          selectedValues={filters.number.map((value) => ({
+            value,
+            display: value,
+          }))}
+          onSelect={(selectedList) =>
+            handleFilterChange("number", selectedList)
+          }
+          onRemove={(selectedList) =>
+            handleFilterChange("number", selectedList)
+          }
+        />
+        <Multiselect
+          options={mod3Values}
+          displayValue="display"
+          placeholder="mod3"
+          selectedValues={filters.mod3.map((value) => ({
+            value,
+            display: value,
+          }))}
+          onSelect={(selectedList) => handleFilterChange("mod3", selectedList)}
+          onRemove={(selectedList) => handleFilterChange("mod3", selectedList)}
+        />
+        <Multiselect
+          options={mod4Values}
+          displayValue="display"
+          placeholder="mod4"
+          selectedValues={filters.mod4.map((value) => ({
+            value,
+            display: value,
+          }))}
+          onSelect={(selectedList) => handleFilterChange("mod4", selectedList)}
+          onRemove={(selectedList) => handleFilterChange("mod4", selectedList)}
+        />
+        <Multiselect
+          options={mod5Values}
+          displayValue="display"
+          placeholder="mod5"
+          selectedValues={filters.mod5.map((value) => ({
+            value,
+            display: value,
+          }))}
+          onSelect={(selectedList) => handleFilterChange("mod5", selectedList)}
+          onRemove={(selectedList) => handleFilterChange("mod5", selectedList)}
+        />
+        <Multiselect
+          options={mod6Values}
+          displayValue="display"
+          placeholder="mod6"
+          selectedValues={filters.mod6.map((value) => ({
+            value,
+            display: value,
+          }))}
+          onSelect={(selectedList) => handleFilterChange("mod6", selectedList)}
+          onRemove={(selectedList) => handleFilterChange("mod6", selectedList)}
+        />
+      </div>
+      <div>
+        <Table tableData={filteredData} />
+      </div>
     </div>
   );
 };
 
-export default DataTableComponent;
+export default Filters;
